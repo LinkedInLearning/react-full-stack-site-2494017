ea72c35326bcbbc93fa30f99245bfef31d0983b9
Sandy Instructor <sandyludosky@gmail.com>
2022-05-24 00:57:13 +0200

    03_07f

--- site-fullstack/index.js
+++ site-fullstack/index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
 import { MongoClient, ServerApiVersion } from 'mongodb';
 const app = express();
 const port = 4000;
 
+dotenv.config();
 
-const uri = "mongodb+srv://sandy:bw26vKuhJ3bwGOmU@fullstack-mern.ehft6.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.STRING_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 client.connect(err => {
