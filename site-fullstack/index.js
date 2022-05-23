ace8e285d091dd20a1173ffbb1d757fd7311406f
Sandy Instructor <sandyludosky@gmail.com>
2022-05-23 16:18:39 +0200

    02_04f

--- site-fullstack/index.js
+++ site-fullstack/index.js
@@ -2,6 +2,10 @@ import express from "express";
 const app = express();
 const port = 4000;
 
+app.get("/", (_, res) => {
+    res.send("Hello Express !")
+})
+
 app.listen(port, () => {
     console.log("serveur démarré avec succès sur le port 4000")
 })
