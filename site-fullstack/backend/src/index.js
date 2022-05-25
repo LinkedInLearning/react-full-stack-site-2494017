407b715af531e0040c1647d721db673a0d83062f
Sandy Instructor <sandyludosky@gmail.com>
2022-05-25 23:01:53 +0200

    08_02f

--- site-fullstack/backend/src/index.js
+++ site-fullstack/backend/src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 const app = express();
+const routes = express.Router()
 const port = 4000;
 
 app.use(cors());
 app.use(express.json());
-require("./routes")(app)
+app.use("/posts", routes);
+require("./routes")(routes)
 
 app.listen(port, () => {
     console.log("serveur démarré avec succès sur le port 4000")
--- site-fullstack/client/src/service/index.js
+++ site-fullstack/client/src/service/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export function getPosts() {
     return new Promise(resolve => {
-        axios.get("http://localhost:4000/")
+        axios.get("http://localhost:4000/posts")
         .then(res => res.status === 200 && res.data)
         .then(resolve)
         .catch(console.error)
@@ -11,7 +11,7 @@ export function getPosts() {
 
 export function insertPost(body) {
     return new Promise(resolve => {
-        axios.post("http://localhost:4000/insert", body)
+        axios.post("http://localhost:4000/posts/insert", body)
         .then(res => res.status === 200 && res.data)
         .then(resolve)
         .catch(console.error)
