87bf753e63854b047411b76ee8080a72d6387ad5
Sandy Instructor <sandyludosky@gmail.com>
2022-05-25 17:08:56 +0200

    07_04f

---
+++ site-fullstack/client/src/context/index.js
@@ -0,0 +1,23 @@
+import { createContext, useContext, useMemo, useState } from "react"
+import { getPosts } from "../service"
+
+const AppContext = createContext();
+const { Provider } = AppContext;
+
+const AppProvider = ({ children}) => {
+    const [posts, setPosts] = useState([]);
+
+    const fetchPosts = () => {
+        getPosts().then(setPosts)
+    }
+
+    const value = useMemo(() => {
+        return {
+            posts, 
+            fetchPosts
+        }
+    }, [posts])
+    return <Provider value={value}>{children}</Provider>
+}
+
+export default AppProvider;
--- site-fullstack/client/src/index.js
+++ site-fullstack/client/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import AppProvider from './context';
 import App from './App';
 import "./App.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+     <AppProvider>
         <App />
+      </AppProvider>
   </React.StrictMode>
 );
