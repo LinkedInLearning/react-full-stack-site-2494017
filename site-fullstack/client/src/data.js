8ae5acb36c9608e52974b6519caaf5fba4293805
Sandy Instructor <sandyludosky@gmail.com>
2022-05-25 09:26:23 +0200

    06_01d

---
+++ site-fullstack/client/src/data.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        _id: 1328932839, 
+        title: "post 1", 
+        content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500"
+    }, 
+    {
+        _id: 23289327893, 
+        title: "post 2", 
+        content: "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+    }, 
+    {
+        _id: 3453784389, 
+        title: "post 3", 
+        content: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."
+    }
+]
